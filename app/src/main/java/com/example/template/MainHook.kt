f975eec56a5e8d7293ed114ab51cec3e00ef4b92
KyuubiRan <KyuubiRan@qq.com>
2022-01-26 00:42:43 +0800

    Update lib version

--- app/src/main/java/com/example/template/MainHook.kt
+++ app/src/main/java/com/example/template/MainHook.kt
@@ -1,16 +1,17 @@
 package com.example.template
 
+import com.example.template.hook.BaseHook
+import com.example.template.hook.ExampleHook
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import com.github.kyuubiran.ezxhelper.utils.Log
 import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.IXposedHookZygoteInit
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-import com.example.template.hook.BaseHook
-import com.example.template.hook.ExampleHook
 
 private const val PACKAGE_NAME_HOOKED = "android"
 private const val TAG = "xposed-template"
 
-class MainHook : IXposedHookLoadPackage {
+class MainHook : IXposedHookLoadPackage, IXposedHookZygoteInit /* Optional */ {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName == PACKAGE_NAME_HOOKED) {
             // Init EzXHelper
@@ -20,6 +21,11 @@ class MainHook : IXposedHookLoadPackage {
             // Init hooks
             initHooks(ExampleHook)
         }
+    }
+
+    // Optional
+    override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
+        EzXHelperInit.initZygote(startupParam)
     }
 
     private fun initHooks(vararg hook: BaseHook) {
