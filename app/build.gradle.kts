bc6e11b7b4d10670ef74094e283dadbf749c1efa
Kitsune <45789471+KyuubiRan@users.noreply.github.com>
2022-05-08 15:56:40 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,6 @@ android {
 }
 
 dependencies {
-    implementation("com.github.kyuubiran:EzXHelper:0.8.8")
+    implementation("com.github.kyuubiran:EzXHelper:0.9.0")
     compileOnly("de.robv.android.xposed:api:82")
 }
